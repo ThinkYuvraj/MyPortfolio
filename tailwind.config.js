96bdc7c69e0755abb5afdc395f5128b9b82aec05
Yuvraj <thinkyuvraj@gmail.com>
2026-08-05 03:33:32 +0530

    Update portfolio

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./*.html"],
+  content: [
+    "./index.html",
+    "./src/**/*.{js,jsx}",
+  ],
   theme: {
   extend: {
     fontFamily: {
